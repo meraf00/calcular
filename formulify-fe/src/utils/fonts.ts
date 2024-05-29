69c1081cadf57226fd3a7d214fef21e9c79775b1
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-05-29 11:44:01 +0300

    feat(formulify-fe): add dependencies

---
+++ formulify-fe/src/utils/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Poppins, Quicksand } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
+
+export const quicksand = Quicksand({ subsets: ['latin'] });
