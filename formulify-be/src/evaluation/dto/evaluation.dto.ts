735d5cd06f9fb35eac96da04e4b856e106ceafae
dawit-getahun-mengistu <dawitgetahunmengistu@gmail.com>
2024-05-30 10:47:16 +0300

    evaluate

--- formulify-be/src/evaluation/dto/evaluation.dto.ts
+++ formulify-be/src/evaluation/dto/evaluation.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class EvaluationDto {
   @ApiProperty({ description: 'The expression to be evaluated' })
-  expression: string;
+  expressionId: string;
 
   @ApiProperty({ description: 'The variable map for the expression' })
-  variableMap: Record<string, any>;
+  variableMap: Record<string, number>;
 }
--- formulify-be/src/evaluation/evaluation.service.ts
+++ formulify-be/src/evaluation/evaluation.service.ts
@@ -3,55 +3,66 @@ import { ExpressionService } from 'src/expression/expression.service';
 import { Variable } from 'src/variable/entities/variable.entity';
 import { VariableService } from 'src/variable/variable.service';
 import { evaluate } from 'mathjs';
+import { Parser } from 'src/expression';
 
 @Injectable()
 export class EvaluationService {
   private variablesList: Variable[] = [];
 
-  constructor(private readonly variableService: VariableService) {}
+  constructor(private readonly expressionService: ExpressionService) {}
 
-  async evaluate(expression: string, variableMap: Record<string, any>) {
+  async evaluate(expressionId: string, variableMap: Record<string, number>) {
     try {
       // validate the expression and get the formula
-      const formula = await this.validate(expression, variableMap);
+      const formula = await this.validate(expressionId, variableMap);
 
       // compute the extracted formula
-      return await this.compute(formula);
+      return await this.compute(formula, variableMap);
     } catch (error) {
       throw new Error(error);
     }
   }
 
-  async validate(expression: string, variableMap: Record<string, any>) {
+  async validate(expressionId: string, variableMap: Record<string, number>) {
     const errors: Error[] = [];
 
+    const expression = (await this.expressionService.findOne(expressionId))
+      .formula;
+
     // validate the variables in  variableMap exist in the expression
-    for (const variableId in variableMap) {
-      if (!expression.includes(variableId)) {
+    for (const variableName in variableMap) {
+      if (!expression.includes(variableName)) {
         errors.push(
-          new Error(`Variable with id ${variableId} not found in expression`),
+          new Error(`Variable with id ${variableName} not found in expression`),
         );
       }
     }
 
-    // validate the variables in  variableMap exist in the database
-    for (const variableId in variableMap) {
-      if (!this.checkVariablesList(variableId)) {
-        const variable = await this.variableService.findOne(variableId);
-        this.variablesList.push(variable);
-
-        if (!variable) {
-          errors.push(
-            new Error(
-              `Invalid Expression: Variable with id ${variableId} not found in database`,
-            ),
-          );
-        }
-      }
-
-      // replace the variable in the expression with its value
-      expression = expression.replace(variableId, variableMap[variableId]);
+    const parser = new Parser(expression);
+    try {
+      parser.validate(Object.keys(variableMap));
+    } catch (error) {
+      errors.push(error);
     }
+
+    // validate the variables in  variableMap exist in the database
+    // for (const variableId in variableMap) {
+    // if (!this.checkVariablesList(variableId)) {
+    // const variable = await this.variableService.findOne(variableId);
+    // this.variablesList.push(variable);
+
+    // if (!variable) {
+    //   errors.push(
+    //     new Error(
+    //       `Invalid Expression: Variable with id ${variableId} not found in database`,
+    //     ),
+    //   );
+    // }
+    // }
+
+    // replace the variable in the expression with its value
+    // expression = expression.replace(variableId, variableMap[variableId]);
+    // }
 
     if (errors.length > 0) {
       for (const error of errors) {
@@ -63,12 +74,12 @@ export class EvaluationService {
   }
 
   // compute the formula
-  compute(formula: string) {
+  compute(formula: string, variableMap: Record<string, number>) {
     try {
-      const result = evaluate(formula);
+      const result = evaluate(formula, variableMap);
       return result;
     } catch (error) {
-      throw new Error(`Error evaluating expression: ${error.message}`);
+      throw new Error(`Error evaluating expression: ${formula}`);
     }
   }
 
